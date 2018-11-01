8bdf71ad85808f64747193957f62857022d7852a
aaditya26 <aaditya.gits@gmail.com>
2018-11-01 06:19:31 +0530

    done

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'atm/index'
+  get 'loan/index'
+  get 'transaction/index'
+  get 'account/index'
+  get 'user/index'
+  get 'bank/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
